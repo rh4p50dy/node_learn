b93bd4ff0eb5f8a5b1d72b01cce971a34a453e2b
rh4p50dy <minsittpaingmhuu16205@gmail.com>
2023-10-29 11:49:10 +0630

    DB Progress

--- utils/database.js
+++ utils/database.js
@@ -2,6 +2,13 @@ const mongodb = require("mongodb")
 const mongodbClient = mongodb.MongoClient
 const dotenv = require("dotenv").config()
 
+let db;
 const connect = ()=>{
-    mongodbClient.connect(process.env.MONGODB_URL).then()
+    mongodbClient.connect(process.env.MONGODB_URL)
+    .then(res => {
+
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 }
